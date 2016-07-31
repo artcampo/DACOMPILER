88931792b00a876cb24a27e096dc5e9a23d3cfad
art <asanemet@bsc.es>
2016-07-31 12:02:42 +0200

    Added README. Updated regenerate_tests script

--- regenerate_tests.sh
+++ regenerate_tests.sh
@@ -2,13 +2,13 @@ cd install/tests
 
 #test1
 ./dacomp test1.bc < test1.txt > test1.out
-cp test1.out test1.ver
-cp test1.bc ../../src/tests/verification/test1.bc.ver
+cp test1.out ../../src/tests/verification/test1.ver
+cp test1.bc  ../../src/tests/verification/test1.bc.ver
 
 #test2
 ./dacomp test2.bc < test2.txt > test2.out
-cp test2.out test2.ver
-cp test2.bc ../../src/tests/verification/test2.bc.ver
+cp test2.out ../../src/tests/verification/test2.ver
+cp test2.bc  ../../src/tests/verification/test2.bc.ver
 
 #end
-echo "Tests regenerated"
+echo "Tests regenerated. You need to reinstall before running tests again."
